58f0bd205335152bea8838dafab4c1020e3ccf5e
MshSiam <siyamkenway@gmail.com>
2022-07-26 11:32:56 +0600

    link navigation

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import Link from "next/link"
+
 const index = () => {
   return (
     <div>
       <h1>This is Home</h1>
+      <Link href="/blog">
+        <a href="">Blog</a>
+      </Link>
+      <Link href="/product">
+        <a href="">Product</a>
+      </Link>
     </div>
   )
 }
